96f73b5b782a5710e9c2a434d6d76f10bf0afd0d
Haemin Park <haemin.parque@gmail.com>
2024-08-15 22:51:17 +0900

    code clean up

--- client/src/components/AuthProvider.tsx
+++ client/src/components/AuthProvider.tsx
@@ -1,59 +1,3 @@
-// import { createContext, useContext, useEffect, useState } from "react";
-// import { useQuery } from "@tanstack/react-query";
-// import axios from "axios";
-
-// interface User {
-//   id: string;
-//   username: string
-// }
-
-// interface AuthContextType {
-//   user: User | null;
-//   isLoading: boolean
-// }
-
-// export const AuthContext = createContext<AuthContextType | undefined>(undefined)
-
-// export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-//   const [user, setUser] = useState<User | null>(null)
-
-//   const { data, isLoading, isError } = useQuery({
-//     queryKey: ["auth"],
-//     queryFn: async () => {
-//       const response = await axios.get("/api/auth/check-auth")
-//       return response.data
-//     },
-//     retry: false
-//   })
-
-
-//   useEffect(() => {
-//     if (data) {
-//       setUser(data)
-//     } else if (isError) {
-//       setUser(null)
-//     }
-//   }, [data, isError])
-
-
-//   console.log(user)
-
-//   return (
-//     <AuthContext.Provider value={{ user, isLoading }}>
-//       {children}
-//     </AuthContext.Provider>
-//   )
-// }
-
-// export const useAuth = () => {
-//   const context = useContext(AuthContext)
-//   if (context === undefined) {
-//     throw new Error("useAuth must be used within an AuthProvider")
-//   }
-
-//   return context
-// }
-
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
 
 import { User } from "../types/User"
@@ -66,7 +10,6 @@ type AuthProviderProps = PropsWithChildren & {
 
 export default function AuthProvider({
   children,
-  // isSignedIn,
 }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
