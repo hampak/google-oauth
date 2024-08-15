2d88292bd6ae3c2248f47af1e5f54e0af5c4ec50
Haemin Park <haemin.parque@gmail.com>
2024-08-15 23:45:51 +0900

    vite.config not working - testing it out this way

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
       <div className="w-[500px] h-[200px] bg-gray-100 shadow-lg rounded-lg py-10 px-4 flex items-center justify-center flex-col space-y-4">
         <h1 className="text-lg font-bold text-center">Sign in with google</h1>
         <button className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition-all">
-          <a href="/api/auth/google" className="flex">
+          <a href="https://server-production-04d8.up.railway.app/api/auth/google" className="flex">
             <KeyRound className="mr-3" /> Sign in with google
           </a>
         </button>
