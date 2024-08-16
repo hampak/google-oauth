1c10e1d5543f1e0d9a4b16e449a4ea3ea9af0b69
Haemin Park <haemin.parque@gmail.com>
2024-08-16 17:25:34 +0900

    can log in now, however, the context doesn't seem to be working

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:8000",
-        target: "https://server-production-04d8.up.railway.app/",
-        changeOrigin: true
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       // target: "http://localhost:8000",
+  //       target: "https://server-production-04d8.up.railway.app/",
+  //       changeOrigin: true
+  //     }
+  //   }
+  // },
   plugins: [react()],
 })
